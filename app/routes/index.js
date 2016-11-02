a5d72cba7fb359d07b4ba04366cf0f51e39afe89
Jeremy Fryd and Tim Bourgault <jeremy+tim@>
2016-11-02 16:18:33 -0700

    functioning search for api call

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+    productLookup(params){
+      this.transitionTo('product', params.product);
+    }
+  }
+
 });
--- app/routes/product.js
+++ app/routes/product.js
@@ -3,14 +3,15 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
-  model: function() {
+  model: function(params) {
+    console.log(params);
     var key = config.myApiKey;
     var url = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords' +
    '&SERVICE-VERSION=1.0.0' +
    '&SECURITY-APPNAME=' + key +
    '&RESPONSE-DATA-FORMAT=JSON' +
    '&REST-PAYLOAD' +
-   '&keywords=harry%20potter%20phoenix';
+   '&keywords=' + params.product;
     return Ember.$.getJSON(url).then(function(responseJSON) {
       console.log(responseJSON.findItemsByKeywordsResponse[0].searchResult[0].item);
       return responseJSON.findItemsByKeywordsResponse[0].searchResult[0].item;
